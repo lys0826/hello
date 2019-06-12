d2b15013bc63feb076d07e73eb3e99de4bb0bf09
lys0826 <2532581846@qq.com>
2019-06-12 08:54:21 +0800

    6.12

---
+++ 4.php
@@ -0,0 +1,25 @@
+<?php
+
+$n = 7;
+// class mash{
+
+	 function calSteps($n){
+
+		for($i=1;$i<$n;$i++){
+			if ($i%2==0) {
+				$a[] = $i;
+			}else {
+				$b[] = $i;
+			}
+		}
+		return $i;
+	}
+echo calSteps($n);
+
+// }
+
+
+
+
+
+
