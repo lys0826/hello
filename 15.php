6062c3629bc8772b1774e691f7a51a0beb21d97f
lys0826 <2532581846@qq.com>
2019-06-25 08:51:14 +0800

    6.25

---
+++ 15.php
@@ -0,0 +1,26 @@
+<?php
+
+$str = 'fdgvbgxesf';
+$sum = strlen($str);
+// print_r($sum);die;
+
+for($i=0;$i<$sum;$i++){
+	$res = substr_count($str);
+	print_r($res);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
